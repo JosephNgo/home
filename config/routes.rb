671b6282c19991fc297a6466d627d119d6b15b46
Joseph Ngo <joengo83@gmail.com>
2012-10-30 14:52:55 -0500

    added new html to make it looks awesome

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Newpicmarks::Application.routes.draw do
   post "/pictures", :controller => 'pictures', :action => 'create'
 
   get "/add_picture", :controller => 'pictures', :action => 'add_picture'
+  
   get "/pictures/:id", :controller => 'Pictures', :action => 'show', :as => 'picture'
   put "/pictures/:id", :controller => 'Pictures', :action => 'update'
   delete "/pictures/:id", :controller => 'Pictures', :action => 'destroy'
+  
   get "/pictures/:id/edit", :controller => 'Pictures', :action => 'edit', :as => 'edit_picture'
   post "/pictures/:id/edit", :controller => 'Pictures', :action => 'edit'
 end
